54f625a856a458fd518fa0783c91726638684d04
RTTArmel <tsiorytah@gmail.com>
2019-07-22 11:59:15 +0300

    rectification

--- src/store/reducer/reducer_admin.js
+++ src/store/reducer/reducer_admin.js
@@ -17,7 +17,7 @@ function connexion(state = initialState, action) {
     case 'REGISTER':
       console.log("action: ", action.value);
       //axios.post('https://shrouded-shore-94366.herokuapp.com/', action.value)
-      axios.post('http://localhost:8080/register/', action.value)
+      axios.post('https://tsiorytahback.herokuapp.com/register/', action.value)
         .then((response) => {
           console.log("post ok: res.data ", response.data);
           localStorage.setItem('id', parseInt(response.data._id))
@@ -33,7 +33,7 @@ function connexion(state = initialState, action) {
       case 'EDIT_PROFIL':
       console.log("action: ", action.value);
       //axios.post('https://shrouded-shore-94366.herokuapp.com/', action.value)
-      axios.put("http://localhost:8080/register/"+localStorage.getItem('id'), action.value)
+      axios.put("https://tsiorytahback.herokuapp.com/register/"+localStorage.getItem('id'), action.value)
         .then((response) => {
           console.log("put ok: res.data ", response.data);
           localStorage.setItem('user', response.data[localStorage.getItem('id')].nom)
@@ -48,7 +48,7 @@ function connexion(state = initialState, action) {
     case 'LOGIN':
       console.log("action: ", action.value);
       //axios.post('https://shrouded-shore-94366.herokuapp.com/', action.value)
-      axios.post('http://localhost:8080/login', action.value)      
+      axios.post('https://tsiorytahback.herokuapp.com/login', action.value)      
         .then((response) => {
           if (response.data.nom == action.value.nom) {
             localStorage.setItem('id', response.data._id)
@@ -71,7 +71,7 @@ function connexion(state = initialState, action) {
 
     case 'GETARTICLE':
       //axios.get('https://shrouded-shore-94366.herokuapp.com/')
-      axios.get('http://localhost:8080/')
+      axios.get('https://tsiorytahback.herokuapp.com/')
         .then(function (response) {
           nextState = response.data
           console.log("ttt", nextState);
